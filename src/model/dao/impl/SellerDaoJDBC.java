653635686f8746ab314a51f59e4920f513172e2e
Cristhiane Jacques <cjn_1707@hotmail.com>
2026-09-12 18:00:48 -0300

    Refactor SellerDaoJDBC to use helper methods for instantiating Seller and Department objects

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -50,27 +50,13 @@ public class SellerDaoJDBC implements SellerDao {
                     + "ON seller.DepartmentId = department.Id "
                     + "WHERE seller.Id = ?"
             );
-
             st.setInt(1, id);
-
             rs = st.executeQuery();
-
             if (rs.next()){ // Quando o ResultSet é criado, ele inicia no posição zero, onde não tem dados, só tem da posição seguinte em diante (se tiver)
                 // Por isso temos que começar verificando se existe um próximo elemento no ResultSet
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-
-                Seller seller = new Seller();
-                seller.setId(rs.getInt("Id"));
-                seller.setName(rs.getString("Name"));
-                seller.setEmail(rs.getString("Email"));
-                seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
-                seller.setBaseSalary(rs.getDouble("BaseSalary"));
-                seller.setDepartment(dep);
-
+                Department dep = instantiateDepartment(rs);
+                Seller seller = instantiateSeller(rs, dep);
                 return seller;
-
             }
             return null; // Se não cair no if do rs.next(), significa que a consulta não retornou nada, então não existe um Seller com o id informado
         } catch (SQLException e) {
@@ -84,5 +70,25 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return List.of();
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller seller = new Seller();
+        seller.setId(rs.getInt("Id"));
+        seller.setName(rs.getString("Name"));
+        seller.setEmail(rs.getString("Email"));
+        seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
+        seller.setBaseSalary(rs.getDouble("BaseSalary"));
+        seller.setDepartment(dep);
+
+        return seller;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+
+        return dep;
     }
 }
