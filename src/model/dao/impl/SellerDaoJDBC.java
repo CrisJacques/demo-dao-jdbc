fb6b5472b091a6381eaff271e55392af86cd4300
Cristhiane Jacques <cjn_1707@hotmail.com>
2026-09-13 15:27:36 -0300

    Implement findAll method in SellerDaoJDBC to retrieve all sellers with their associated departments

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -72,7 +72,42 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "ORDER BY Name"
+            );
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>(); // map que vai armazenar todos os Department que forem instanciados
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId")); // verificando se o Department já existe, para só instanciar se ainda não existir
+
+                if (dep == null) {// quando o map.get() não encontra o elemento buscado, ele retorna null
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = instantiateSeller(rs, dep);
+                list.add(seller);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+
+        }
     }
 
     @Override
